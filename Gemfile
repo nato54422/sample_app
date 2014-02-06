f0ca38d8039f6e0558b404e47940e5841f9a63ca
NaokiTaguchi <nato54422@gmail.com>
2014-02-06 18:09:01 +0900

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -7,11 +7,16 @@ gem 'rails', '4.0.2'
 group :development, :test do
 	gem 'sqlite3'
 	gem 'rspec-rails'
+	gem 'guard-rspec'
+	gem 'spork-rails'
+	gem 'guard-spork'
+	gem 'childprocess'
 end
 
 group :test do
 	gem 'selenium-webdriver', '2.35.1'
 	gem 'capybara', '2.1.0'
+	gem 'growl'
 end
 
 # Use SCSS for stylesheets
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,13 @@ describe "StaticPages" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
-		it "should have the title 'Home'" do
+		it "should have the base title" do
 			visit '/static_pages/home'
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			expect(page).not_to_have_title('| Home')
 		end
 	end
 
@@ -35,4 +39,14 @@ describe "StaticPages" do
 		end
 	end
 
+	describe "Contact page" do
+		it "should have the content 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_content('Contact')
+		end
+		it "should have the title 'Contact'" do
+			visit '/static_pages/contact'
+			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+		end
+	end
 end
